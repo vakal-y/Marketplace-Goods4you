fbd8276da80adc30b9195f9c5ebe2e8ff82ab9e2
vakal.y <dugintseva.y@yandex.ru>
2024-07-02 15:36:06 +0300

    add styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,20 @@
 import { Link } from 'react-router-dom';
-import './Header.scss';
+import styles from './Header.module.scss';
+import cart from '../../assets/cart.svg';
 
 const Header: React.FC = () => {
     return (
-        <header className="header">
-            <Link to="/">Home</Link>
-            <Link to="/product">Product</Link>
+        <header className={styles.header}>
+            <Link to="/" className={styles.logo}>Goods4you</Link>
+            <nav className={styles.nav}>
+                <Link to="/catalog">Catalog</Link>
+                <Link to="/faq">FAQ</Link>
+                <Link to="/cart" className={styles.cart}>
+                    Cart
+                    <img className={styles.img} src={cart} alt="cart" />
+                </Link>
+                <Link to="/user-dashboard">Личный кабинет</Link>
+            </nav>
         </header>
     );
 };
